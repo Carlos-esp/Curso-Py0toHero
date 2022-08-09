04c9cbe36353e2b70e3fd1cf361e23b7f1bda30f
Carlos-esp <Carlos_aeb@hotmail.com>
2022-08-08 22:30:57 -0500

    first commit

---
+++ HW/Tarea1.py
@@ -0,0 +1,14 @@
+#PROGRAMA QUE RECOPILE NOMBRE, APELLIDO, EDAD, SEXO, OCUPACION Y SALARIO DE UN USUARIO.
+#IMPRIMIAN cada una de las variables!
+
+
+Nombre=input("Por favor, introduce tu nombre: ")
+Apellido=input("Por favor, introduce tu apellido paterno: ")
+Edad=input("Por favor, introduce tu edad: ")
+Sexo=input("Por favor, introduce tu sexo: M/F: ")
+Ocupacion=input("Por favor, introduce tu ocupación: ")
+Salario=input("Por favor, introduce un aproximado de tu sueldo mensual: ")
+
+print("Hola " + Nombre + " " + Apellido)
+print("Eres una persona del sexo: "+ Sexo + ", con edad: "+Edad)
+print("Tu ocupación es: " + Ocupacion +", con un sueldo aproximado de: "+ Salario)
---
+++ HW/Tarea3_blackjack.py
@@ -0,0 +1,34 @@
+#Simulación de un juego de blackjack con 2 mazos
+import numpy as np
+
+mazo=["A",1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+
+def jugar_blackj(mazo, num_mazo):
+    mazo_tot=mazo*num_mazo
+    cartas=np.random.choice(mazo_tot,size=4,replace=False)
+    print(f'Las cartas elegidas son: {cartas}')
+    for i,j in enumerate(cartas):
+        if j in ["J","Q","K"]:
+            cartas[i]=10
+        elif j in ["A"]:
+            cartas[i]=1
+        else:
+            cartas[i]=int(j)
+    cartas=cartas.astype(int)
+    mano_jug=cartas[[0,2]]
+    mano_casa=cartas[[1,3]]
+    puntos_jug=sum(mano_jug)
+    puntos_casa=sum(mano_casa)
+    print(f'La mano del jugador es {mano_jug}, puntos totales: {puntos_jug}')
+    print(f'La mano de la casa es {mano_casa}, puntos totales: {puntos_casa}')
+    if puntos_jug>puntos_casa:
+        ganador="Jugador"
+    elif puntos_jug<puntos_casa:
+        ganador="Casa"
+    else:
+        ganador="Empate"
+    print(ganador)
+    return ganador
+
+
+jugar_blackj(mazo,2)
